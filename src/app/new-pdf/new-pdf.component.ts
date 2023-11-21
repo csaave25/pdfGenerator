a6be48b06182fe9f619ff02d9a5ccd133da4040e
csaave25 <csaavedra@emt.cl>
2023-11-21 18:03:12 -0300

    format emt

--- src/app/new-pdf/new-pdf.component.ts
+++ src/app/new-pdf/new-pdf.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule, FormGroup } from '@angular/forms';
 import { jsPDF } from 'jspdf'
 import html2canvas from 'html2canvas';
+import { PdfGenerateService } from '../pdf-generate.service';
+import { from } from 'rxjs';
 
 
 @Component({
@@ -11,6 +13,7 @@ import html2canvas from 'html2canvas';
 })
 export class NewPDFComponent {
 
+  constructor(private generaService: PdfGenerateService) { }
 
 
   export = false
@@ -20,29 +23,29 @@ export class NewPDFComponent {
     observaciones: new FormControl('Sin observaciones'),
     zona: new FormControl(''),
     pared: new FormControl(null),
-    este: new FormControl(null),
-    norte: new FormControl(null),
-    cota: new FormControl(null),
+    este: new FormControl(NaN),
+    norte: new FormControl(NaN),
+    cota: new FormControl(NaN),
     produccion: new FormControl(false),
     precorte: new FormControl(false),
     destape: new FormControl(false),
     bolones: new FormControl(false),
     desquinche: new FormControl(false),
     contorno: new FormControl(false),
-    pozosProduccion: new FormControl(null),
-    nMallas: new FormControl(null),
-    pozosPrecorte: new FormControl(null),
-    factorCarga: new FormControl(null),
-    Volumne: new FormControl(null),
+    pozosProduccion: new FormControl(NaN),
+    nMallas: new FormControl(NaN),
+    pozosPrecorte: new FormControl(NaN),
+    factorCarga: new FormControl(NaN),
+    Volumne: new FormControl(NaN),
     mapaSintetico: new FormControl(null),
     graficoDesplazaminetotiempo: new FormControl(null),
-    radar: new FormControl(null),
-    descripcionPunto: new FormControl(null),
-    radarEste: new FormControl(null),
-    radarNorte: new FormControl(null),
-    radarCota: new FormControl(null),
-    deplazamiento4horas: new FormControl(null),
-    velocidadPromedio: new FormControl(null),
+    radar: new FormControl(''),
+    descripcionPunto: new FormControl(NaN),
+    radarEste: new FormControl(NaN),
+    radarNorte: new FormControl(NaN),
+    radarCota: new FormControl(NaN),
+    deplazamiento4horas: new FormControl(NaN),
+    velocidadPromedio: new FormControl(NaN),
     posicionTronadura: new FormControl(null),
     antesPanoramica: new FormControl(null),
     despuesPanoramica: new FormControl(null),
@@ -56,19 +59,9 @@ export class NewPDFComponent {
 
   changeState() {
     this.export = !this.export
+
   }
 
-  createTable() {
-
-    let form = this.reporte.controls
-    let tab = []
-    // produccion: new FormControl(false),
-    // precorte: new FormControl(false),
-    // destape: new FormControl(false),
-    // bolones: new FormControl(false),
-    // desquinche: new FormControl(false),
-    // contorno:
-  }
 
   getFecha(): string {
 
@@ -92,198 +85,22 @@ export class NewPDFComponent {
     let ano = new Date(form.fecha.value!).getFullYear()
     return dia + ' ' + diaNum + ' de ' + mes + ' de ' + ano + ', ' + hora + ' hrs.'
 
-
   }
 
 
   generatePDF() {
-
     let form = this.reporte.controls
-    let startY = 10
-    let endY = 835
-    let left = 30
-    let right = 570
-    let midPointX = (right + left) / 2
-
-    let doc = new jsPDF('p', 'pt', 'a4')
-
-    //HEADER
-    doc.setFontSize(13)
-    doc.setFont("helvetica")
-    doc.text("REPORTE PUNTOS DE SEGUIMIENTO POST TRONADURA", midPointX, 40, { align: 'center' });
-    doc.setFont("helvetica");
-    doc.setFontSize(11)
-    doc.text("TRONADURA " + form.zona.value!.toUpperCase() + " - COORDENADAS: " + form.este.value + " | " + form.norte.value + " | " + form.cota.value, midPointX, 55, { align: 'center' });
-    doc.text(this.getFecha(), midPointX, 70, { align: 'center' });
-    doc.text("2023.12", left, 80, { align: 'left' });
-    doc.text("IYV-RP-VIG-CMZ-04", right, 80, { align: 'right' });
-    doc.line(left, 85, right, 85);
-
-
-    //midINFO
-    doc.setFontSize(12)
-    doc.setFont("helvetica", "bold");
-    doc.text("PLANO DE UBICACIÓN", left, 110, { align: 'left' });
-    doc.addImage("assets/img1.png", 'PNG', 130, 130, 340, 360);
-    doc.setFontSize(8)
-    doc.setFont("helvetica", "normal");
-    doc.text("Figura 1: Posición de tronadura e instrumentación", midPointX, 500, { align: 'center' });
-
-    doc.setFontSize(12)
-    doc.setFont("helvetica", "bold");
-    doc.text("INFORMACIÓN TRONADURA", 30, 540, { align: 'left' });
-    doc.line(left, 560, right, 560);
-    doc.setFontSize(10)
-
-    let calc = (right + left) / 7
-    doc.text("Producción", calc, 590, { align: 'center' });
-    doc.text("Precorte", calc * 2, 590, { align: 'center' });
-    doc.text("Destape", calc * 3, 590, { align: 'center' });
-    doc.text("Bolones", calc * 4, 590, { align: 'center' });
-    doc.text("Desquinche", calc * 5, 590, { align: 'center' });
-    doc.text("Contorno", calc * 6, 590, { align: 'center' });
-    let tamañoImg = 7
-    form.produccion.value ? doc.addImage("../../assets/check.png", 'PNG', calc - 5, 600, tamañoImg, tamañoImg) : doc.addImage("../../assets/ximg.png", 'PNG', calc - 5, 600, tamañoImg, tamañoImg);
-    form.precorte.value ? doc.addImage("../../assets/check.png", 'PNG', calc * 2 - 5, 600, tamañoImg, tamañoImg) : doc.addImage("../../assets/ximg.png", 'PNG', calc * 2 - 5, 600, tamañoImg, tamañoImg);
-    form.destape.value ? doc.addImage("../../assets/check.png", 'PNG', calc * 3 - 5, 600, tamañoImg, tamañoImg) : doc.addImage("../../assets/ximg.png", 'PNG', calc * 3 - 5, 600, tamañoImg, tamañoImg);
-    form.bolones.value ? doc.addImage("../../assets/check.png", 'PNG', calc * 4 - 5, 600, tamañoImg, tamañoImg) : doc.addImage("../../assets/ximg.png", 'PNG', calc * 4 - 5, 600, tamañoImg, tamañoImg);
-    form.desquinche.value ? doc.addImage("../../assets/check.png", 'PNG', calc * 5 - 5, 600, tamañoImg, tamañoImg) : doc.addImage("../../assets/ximg.png", 'PNG', calc * 5 - 5, 600, tamañoImg, tamañoImg);
-    form.contorno.value ? doc.addImage("../../assets/check.png", 'PNG', calc * 6 - 5, 600, tamañoImg, tamañoImg) : doc.addImage("../../assets/ximg.png", 'PNG', calc * 6 - 5, 600, tamañoImg, tamañoImg);
-
-    doc.line(left, 635, right, 635);
-    calc = (right + left) / 4
-    doc.text("N° Pozos Producción: " + form.pozosProduccion, calc, 665, { align: 'center' });
-    doc.text("N° Pozos Precorte: " + form.pozosPrecorte, calc * 2, 665, { align: 'center' });
-    doc.text("N° de Mallas: " + form.nMallas, calc * 3, 665, { align: 'center' });
-    doc.line(left, 685, right, 685);
-    calc = (right + left) / 3
-    doc.text("Volumen (Kton): " + form.Volumne, calc, 715, { align: 'center' });
-    doc.text("Factor de Carga General: " + form.factorCarga, calc * 2, 715, { align: 'center' });
-
-
-    //FOOTER
-    doc.setFontSize(10)
-    doc.text("1", midPointX, endY, { align: 'center' });
-    doc.text("Néstor Ramírez Muena", 30, endY, { align: 'left' });
-    doc.text("2023.12", 570, endY, { align: 'right' });
-
-
-    //NUEVA PAGINA  
-    doc.addPage()
-
-
-    //HEADER
-    doc.setFontSize(13)
-    doc.setFont("helvetica")
-    doc.text("REPORTE PUNTOS DE SEGUIMIENTO POST TRONADURA", midPointX, 40, { align: 'center' });
-    doc.setFont("helvetica");
-    doc.setFontSize(11)
-    doc.text("TRONADURA " + form.zona.value!.toUpperCase() + " - COORDENADAS: " + form.este.value + " | " + form.norte.value + " | " + form.cota.value, midPointX, 55, { align: 'center' });
-    doc.text(this.getFecha(), midPointX, 70, { align: 'center' });
-    doc.text("2023.12", left, 80, { align: 'left' });
-    doc.text("IYV-RP-VIG-CMZ-04", right, 80, { align: 'right' });
-    doc.line(left, 85, right, 85);
-
-
-    //MID
-
-    doc.setFontSize(12)
-    doc.setFont("helvetica", "bold");
-    doc.text("PUNTOS DE CONTROL SECTOR TRONADURA", left, 110, { align: 'left' });
-    let midIMG = 110 + left
-    doc.addImage("assets/img1.png", 'PNG', midIMG, 125, 320, 220);
-    doc.setFontSize(8)
-    doc.setFont("helvetica", "normal");
-    doc.text("Figura 2: Mapa sintético de desplazamientos Vs. tiempo", midPointX, 355, { align: 'center' });
-
-
-
-    doc.addImage("../../assets/img1.png", 'PNG', midIMG, 380, 320, 220);
-    doc.setFontSize(8)
-    doc.setFont("helvetica", "normal");
-    doc.text("Figura 3: Gráfico de desplazamientos Vs. tiempo", midPointX, 610, { align: 'center' });
-
-    doc.line(left, 630, right, 630);
-    doc.setFont("helvetica", "bold");
-    doc.setFontSize(9)
-    calc = (right + left) / 8
-    doc.text("Radar", calc, 650, { align: 'center' });
-    doc.text("Descripción del", calc * 2, 645, { align: 'center' });
-    doc.text("Punto", calc * 2, 655, { align: 'center' });
-    doc.text("Este", calc * 3, 650, { align: 'center' });
-    doc.text("Norte", calc * 4, 650, { align: 'center' });
-    doc.text("Cota", calc * 5, 650, { align: 'center' });
-    doc.text("Desplazamiento", calc * 6, 645, { align: 'center' });
-    doc.text("(4hrs.)", calc * 6, 655, { align: 'center' });
-    doc.text("Velocidad", calc * 7, 645, { align: 'center' });
-    doc.text("(4hrs.)", calc * 7, 655, { align: 'center' });
-    doc.setFont("helvetica", "normal");
-    doc.line(left, 668, right, 668);
-    //radares
-    doc.text("IBIS ArcSAR", calc, 680, { align: 'center' });
-    doc.text("138", calc * 2, 680, { align: 'center' });
-    doc.text("93572", calc * 3, 680, { align: 'center' });
-    doc.text("22485", calc * 4, 680, { align: 'center' });
-    doc.text("3149", calc * 5, 680, { align: 'center' });
-    doc.text("-2,48mm", calc * 6, 680, { align: 'center' });
-    doc.text("-0,63mm/h", calc * 7, 680, { align: 'center' });
-
-    //FOOTER
-    doc.setFontSize(10)
-    doc.text("2", midPointX, endY, { align: 'center' });
-    doc.text("Néstor Ramírez Muena", 30, endY, { align: 'left' });
-    doc.text("2023.12", 570, endY, { align: 'right' });
-
-
-
-    //NUEVA PAGINA  
-    doc.addPage()
-
-    //HEADER
-    doc.setFontSize(13)
-    doc.setFont("helvetica")
-    doc.text("REPORTE PUNTOS DE SEGUIMIENTO POST TRONADURA", midPointX, 40, { align: 'center' });
-    doc.setFont("helvetica");
-    doc.setFontSize(11)
-    doc.text("TRONADURA " + form.zona.value!.toUpperCase() + " - COORDENADAS: " + form.este.value + " | " + form.norte.value + " | " + form.cota.value, midPointX, 55, { align: 'center' });
-    doc.text(this.getFecha(), midPointX, 70, { align: 'center' });
-    doc.text("2023.12", left, 80, { align: 'left' });
-    doc.text("IYV-RP-VIG-CMZ-04", right, 80, { align: 'right' });
-    doc.line(left, 85, right, 85);
-
-
-    //MID
-
-    doc.setFontSize(12)
-    doc.setFont("helvetica", "bold");
-    doc.text("IMÁGENES", left, 110, { align: 'left' });
-    midIMG = 110 + left
-    doc.addImage("assets/img1.png", 'PNG', left, 120, 260, 180);
-    doc.addImage("assets/img1.png", 'PNG', right - 260, 120, 260, 180);
-    doc.setFontSize(8)
-    doc.setFont("helvetica", "normal");
-    doc.text("Figura 4: Antes Panorámica ", 260/2 +left, 310, { align: 'center' });
-    doc.text("Figura 5: Después Panorámica", right - 260/2, 310, { align: 'center' });
-
-
-
-    //FOOTER
-    doc.setFontSize(10)
-    doc.text("2", midPointX, endY, { align: 'center' });
-    doc.text("Néstor Ramírez Muena", 30, endY, { align: 'left' });
-    doc.text("2023.12", 570, endY, { align: 'right' });
-
-
-    //ABRIR VENTANA NUEVA
-    var string = doc.output('datauristring');
-    var embed = "<embed width='100%' height='100%' src='" + string + "'/>"
-    var x = window.open();
-    x!.document.open();
-    x!.document.write(embed);
-    x!.document.body.style.margin = '0'
-    x!.document.close();
-    //doc.save('intento1')
-
+    this.generaService.tampleEMT()
+    this.generaService.doc.addPage()
+    this.generaService.generateHeader(form.zona.value!, form.este.value!, form.norte.value!, form.cota.value!, this.getFecha())
+    this.generaService.generateFooter()
+    this.generaService.planoDeUbicacion()
+    this.generaService.informacionTronadura(form.produccion.value!, form.precorte.value!, form.destape.value!, form.bolones.value!,
+      form.desquinche.value!, form.contorno.value!, form.pozosProduccion.value!, form.pozosPrecorte.value!, form.nMallas.value!,
+      form.Volumne.value!, form.factorCarga.value!)
+    this.generaService.controlSector(form.radar.value!, form.descripcionPunto.value!, form.radarEste.value!, form.radarNorte.value!, form.radarCota.value!, form.deplazamiento4horas.value!, form.velocidadPromedio.value!)
+    this.generaService.generarImagenes()
+    this.generaService.previsualizar()
   }
 
 
---
+++ src/app/pdf-generate.service.ts
@@ -0,0 +1,286 @@
+import { Injectable } from '@angular/core';
+import { jsPDF } from 'jspdf'
+import { radares } from './dataPrueba';
+
+
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PdfGenerateService {
+
+  constructor() { }
+
+  rds = radares
+
+  startY = 10
+  endY = 835
+  left = 30
+  right = 570
+  midPointX = (this.right + this.left) / 2
+  midPointY = (this.startY + this.endY) / 2
+  totalY = this.endY - this.startY
+  numFigura = 1
+  usoPage = 0
+
+
+  doc = new jsPDF('p', 'pt', 'A4')
+
+
+
+  generateNewPage(espacio: number) {
+    let zona = ''
+    let este = 22
+    let norte = 22
+    let cota = 22
+    let fecha = new Date().toLocaleString()
+    if ((this.totalY - this.usoPage) < espacio) {
+      this.doc.addPage()
+      this.generateHeader(zona, este, norte, cota, fecha)
+      this.generateFooter()
+    }
+  }
+
+
+
+  generateHeader(zona: string, este: number, norte: number, cota: number, fecha: string) {
+
+    this.doc.setFontSize(13)
+    this.doc.setFont("helvetica")
+    this.doc.text("REPORTE PUNTOS DE SEGUIMIENTO POST TRONADURA", this.midPointX, 40, { align: 'center' });
+    this.doc.setFont("helvetica");
+    this.doc.setFontSize(11)
+    this.doc.text("TRONADURA " + zona.toUpperCase() + " - COORDENADAS: " + este + " | " + norte + " | " + cota, this.midPointX, 55, { align: 'center' });
+    this.doc.text(fecha, this.midPointX, 70, { align: 'center' });
+    this.doc.text("2023.12", this.left, 80, { align: 'left' });
+    this.doc.text("IYV-RP-VIG-CMZ-04", this.right, 80, { align: 'right' });
+    this.doc.line(this.left, 85, this.right, 85);
+    this.usoPage = 110
+
+  }
+
+  generateFooter() {
+    this.doc.setFontSize(10)
+    this.doc.text('1', this.midPointX, this.endY, { align: 'center' });
+    this.doc.text("Néstor Ramírez Muena", 30, this.endY, { align: 'left' });
+    this.doc.text("2023.12", 570, this.endY, { align: 'right' });
+  }
+
+  planoDeUbicacion() {
+    this.doc.setFontSize(12)
+    this.doc.setFont("helvetica", "bold");
+    this.doc.text("PLANO DE UBICACIÓN", this.left, 110, { align: 'left' });
+    this.doc.addImage("assets/img1.png", "PNG", this.left + 100, 130, 340, 360, "alias1", 'SLOW');
+    this.doc.setFontSize(8)
+    this.doc.setFont("helvetica", "normal");
+    this.doc.text("Figura " + this.numFigura + ": Posición de tronadura e instrumentación", this.midPointX, 500, { align: 'center' });
+    this.numFigura++
+    this.usoPage += 430
+  }
+
+  informacionTronadura(produccion: boolean, precorte: boolean, destape: boolean, bolones: boolean, desquinche: boolean, contorno: boolean,
+    pozosProduccion: number, pozosPrecorte: number, nMallas: number, volumen: number, factorCarga: number) {
+
+    this.doc.setFontSize(12)
+    this.doc.setFont("helvetica", "bold");
+    this.doc.text("INFORMACIÓN TRONADURA", 30, this.usoPage, { align: 'left' });
+    this.doc.line(this.left, this.usoPage + 20, this.right, this.usoPage + 20);
+    this.doc.setFontSize(10)
+    let calc = (this.right + this.left) / 7
+    this.doc.text("Producción", calc, this.usoPage + 35, { align: 'center' });
+    this.doc.text("Precorte", calc * 2, this.usoPage + 35, { align: 'center' });
+    this.doc.text("Destape", calc * 3, this.usoPage + 35, { align: 'center' });
+    this.doc.text("Bolones", calc * 4, this.usoPage + 35, { align: 'center' });
+    this.doc.text("Desquinche", calc * 5, this.usoPage + 35, { align: 'center' });
+    this.doc.text("Contorno", calc * 6, this.usoPage + 35, { align: 'center' });
+    let tamañoImg = 7
+    let alturaY = this.usoPage + 50
+
+    produccion ? this.doc.addImage("../../assets/check.png", 'PNG', calc - 5, alturaY, tamañoImg, tamañoImg, 'icon1', 'SLOW') : this.doc.addImage("../../assets/ximg.png", 'PNG', calc - 5, alturaY, tamañoImg, tamañoImg, 'icon1', 'SLOW');
+    precorte ? this.doc.addImage("../../assets/check.png", 'PNG', calc * 2 - 5, alturaY, tamañoImg, tamañoImg, 'icon2', 'SLOW') : this.doc.addImage("../../assets/ximg.png", 'PNG', calc * 2 - 5, alturaY, tamañoImg, tamañoImg, 'icon2', 'SLOW');
+    destape ? this.doc.addImage("../../assets/check.png", 'PNG', calc * 3 - 5, alturaY, tamañoImg, tamañoImg, 'icon3', 'SLOW') : this.doc.addImage("../../assets/ximg.png", 'PNG', calc * 3 - 5, alturaY, tamañoImg, tamañoImg, 'icon3', 'SLOW');
+    bolones ? this.doc.addImage("../../assets/check.png", 'PNG', calc * 4 - 5, alturaY, tamañoImg, tamañoImg, 'icon4', 'SLOW') : this.doc.addImage("../../assets/ximg.png", 'PNG', calc * 4 - 5, alturaY, tamañoImg, tamañoImg, 'icon4', 'SLOW');
+    desquinche ? this.doc.addImage("../../assets/check.png", 'PNG', calc * 5 - 5, alturaY, tamañoImg, tamañoImg, 'icon5', 'SLOW') : this.doc.addImage("../../assets/ximg.png", 'PNG', calc * 5 - 5, alturaY, tamañoImg, tamañoImg, 'icon5', 'SLOW');
+    contorno ? this.doc.addImage("../../assets/check.png", 'PNG', calc * 6 - 5, alturaY, tamañoImg, tamañoImg, 'icon6', 'SLOW') : this.doc.addImage("../../assets/ximg.png", 'PNG', calc * 6 - 5, alturaY, tamañoImg, tamañoImg, 'icon6', 'SLOW');
+
+
+    this.doc.line(this.left, alturaY + 20, this.right, alturaY + 20);
+    calc = (this.right + this.left) / 4
+    this.doc.text("N° Pozos Producción: ", calc, alturaY + 40, { align: 'center' });
+    this.doc.setFont("helvetica", "normal");
+    this.doc.text(pozosProduccion.toString(), calc + 65, alturaY + 40, { align: 'center' });
+    this.doc.setFont("helvetica", "bold");
+    this.doc.text("N° Pozos Precorte: ", calc * 2, alturaY + 40, { align: 'center' });
+    this.doc.setFont("helvetica", "normal");
+    this.doc.text(pozosPrecorte.toString(), calc * 2 + 57, alturaY + 40, { align: 'center' });
+    this.doc.setFont("helvetica", "bold");
+    this.doc.text("N° de Mallas: ", calc * 3, alturaY + 40, { align: 'center' });
+    this.doc.setFont("helvetica", "normal");
+    this.doc.text(nMallas.toString(), calc * 3 + 43, alturaY + 40, { align: 'center' });
+    calc = (this.right + this.left) / 3
+    this.doc.line(this.left, alturaY + 55, this.right, alturaY + 55);
+    this.doc.text("Volumen (Kton): " , calc, alturaY + 75, { align: 'center' });
+    this.doc.text(volumen.toString(), calc + 20, alturaY + 75, { align: 'center' });
+    this.doc.text("Factor de Carga General: " + factorCarga, calc * 2, alturaY + 75, { align: 'center' });
+    // doc.text(form.factorCarga.value!?.toString(), calc * 2, 715, { align: 'center' });
+
+    this.usoPage = alturaY + 95
+
+  }
+
+  controlSector(radar: string, descripcionPunto: number, radarEste: number, radarNorte: number, radarCota: number, deplazamiento4horas: number, velocidadPromedio: number) {
+    this.usoPage += 20
+    this.generateNewPage(220)
+    this.doc.setFontSize(12)
+    this.doc.setFont("helvetica", "bold");
+    this.doc.text("PUNTOS DE CONTROL SECTOR TRONADURA", this.left, this.usoPage, { align: 'left' });
+    let midIMG = 110 + this.left
+
+
+    //1ra Img
+    this.doc.addImage("assets/img/mapa1.png", 'PNG', midIMG, this.usoPage + 15, 320, 220, 'figura2', 'SLOW');
+    this.doc.setFontSize(8)
+    this.doc.setFont("helvetica", "normal");
+    this.doc.text("Figura 2: Mapa sintético de desplazamientos Vs. tiempo", this.midPointX, this.usoPage + 245, { align: 'center' });
+    this.usoPage += 230
+
+    //2da Img
+    this.generateNewPage(220)
+    this.doc.addImage("../../assets/img/mapa2.png", 'PNG', midIMG, this.usoPage + 25, 320, 220, 'figura 3', 'SLOW');
+    this.doc.setFontSize(8)
+    this.doc.setFont("helvetica", "normal");
+    this.doc.text("Figura 3: Gráfico de desplazamientos Vs. tiempo", this.midPointX, this.usoPage + 255, { align: 'center' });
+    this.usoPage += 230
+
+
+    //TABLA
+    this.usoPage += 50
+    this.generateNewPage(50)
+    this.doc.line(this.left, this.usoPage, this.right, this.usoPage);
+    this.doc.setFont("helvetica", "bold");
+    this.doc.setFontSize(9)
+    let calc = (this.right + this.left) / 8
+    this.doc.text("Radar", calc, this.usoPage + 20, { align: 'center' });
+    this.doc.text("Descripción del", calc * 2, this.usoPage + 15, { align: 'center' });
+    this.doc.text("Punto", calc * 2, this.usoPage + 25, { align: 'center' });
+    this.doc.text("Este", calc * 3, this.usoPage + 20, { align: 'center' });
+    this.doc.text("Norte", calc * 4, this.usoPage + 20, { align: 'center' });
+    this.doc.text("Cota", calc * 5, this.usoPage + 20, { align: 'center' });
+    this.doc.text("Desplazamiento", calc * 6, this.usoPage + 15, { align: 'center' });
+    this.doc.text("(4hrs.)", calc * 6, this.usoPage + 25, { align: 'center' });
+    this.doc.text("Velocidad", calc * 7, this.usoPage + 15, { align: 'center' });
+    this.doc.text("(4hrs.)", calc * 7, this.usoPage + 25, { align: 'center' });
+    this.doc.setFont("helvetica", "normal");
+    this.doc.line(this.left, this.usoPage + 38, this.right, this.usoPage + 38);
+    //radares
+
+    this.usoPage += 50
+    this.rds.forEach(rdr => {
+      this.doc.setFontSize(9)
+      this.doc.setFont("helvetica", "normal");
+      // this.doc.text(radar, calc, this.usoPage, { align: 'center' });
+      // this.doc.text(descripcionPunto.toString(), calc * 2, this.usoPage, { align: 'center' });
+      // this.doc.text(radarEste.toString(), calc * 3, this.usoPage, { align: 'center' });
+      // this.doc.text(radarNorte.toString(), calc * 4, this.usoPage, { align: 'center' });
+      // this.doc.text(radarCota.toString(), calc * 5, this.usoPage, { align: 'center' });
+      // this.doc.text(deplazamiento4horas + "mm", calc * 6, this.usoPage, { align: 'center' });
+      // this.doc.text(velocidadPromedio + "mm/h", calc * 7, this.usoPage, { align: 'center' });
+      this.doc.text(rdr.radar, calc, this.usoPage, { align: 'center' });
+      this.doc.text(rdr.descripcion.toString(), calc * 2, this.usoPage, { align: 'center' });
+      this.doc.text(rdr.este.toString(), calc * 3, this.usoPage, { align: 'center' });
+      this.doc.text(rdr.norte.toString(), calc * 4, this.usoPage, { align: 'center' });
+      this.doc.text(rdr.norte.toString(), calc * 5, this.usoPage, { align: 'center' });
+      this.doc.text(rdr.desplazamiento + "mm", calc * 6, this.usoPage, { align: 'center' });
+      this.doc.text(rdr.velocidad + "mm/h", calc * 7, this.usoPage, { align: 'center' });
+      this.usoPage += 12
+      this.generateNewPage(10)
+    })
+  }
+
+
+  generarImagenes() {
+    this.generateNewPage(190)
+    this.usoPage += 20
+    this.doc.setFontSize(12)
+    this.doc.setFont("helvetica", "bold");
+    this.doc.text("IMÁGENES", this.left, this.usoPage, { align: 'left' });
+    let midIMG = 110 + this.left
+
+    this.doc.setFontSize(8)
+    this.doc.setFont("helvetica", "normal");
+    //Primera img
+    this.doc.addImage("assets/img1.png", 'PNG', this.left, this.usoPage + 10, 260, 180, 'alias1', 'SLOW');
+    this.doc.text("Figura 4: Antes Panorámica ", 260 / 2 + this.left, this.usoPage + 200, { align: 'center' });
+
+    //segunda img
+    this.doc.addImage("assets/img1.png", 'PNG', this.right - 260, this.usoPage + 10, 260, 180, 'alias1', 'SLOW');
+    this.doc.text("Figura 5: Después Panorámica", this.right - 260 / 2, this.usoPage + 200, { align: 'center' });
+
+
+    this.usoPage += 210
+    this.generateNewPage(260)
+
+    //3ra img
+    this.doc.addImage("assets/img1.png", 'PNG', this.left, this.usoPage + 10, 260, 180, 'alias1', 'SLOW');
+    this.doc.text("Figura 4: Antes Panorámica ", 260 / 2 + this.left, this.usoPage + 200, { align: 'center' });
+
+    //4ta img
+    this.doc.addImage("assets/img1.png", 'PNG', this.right - 260, this.usoPage + 10, 260, 180, 'alias1', 'SLOW');
+    this.doc.text("Figura 5: Después Panorámica", this.right - 260 / 2, this.usoPage + 200, { align: 'center' });
+
+    this.usoPage += 210
+    this.generateNewPage(260)
+
+    //5ra img
+    this.doc.addImage("assets/img1.png", 'PNG', this.left, this.usoPage + 10, 260, 180, 'alias1', 'SLOW');
+    this.doc.text("Figura 4: Antes Panorámica ", 260 / 2 + this.left, this.usoPage + 200, { align: 'center' });
+
+    //6ta img
+    this.doc.addImage("assets/img1.png", 'PNG', this.right - 260, this.usoPage + 10, 260, 180, 'alias1', 'SLOW');
+    this.doc.text("Figura 5: Después Panorámica", this.right - 260 / 2, this.usoPage + 200, { align: 'center' });
+
+    this.usoPage += 210
+    this.generateNewPage(260)
+
+    //7ra img
+    this.doc.addImage("assets/img1.png", 'PNG', this.left, this.usoPage + 10, 260, 180, 'alias1', 'SLOW');
+    this.doc.text("Figura 4: Antes Panorámica ", 260 / 2 + this.left, this.usoPage + 200, { align: 'center' });
+
+    //8ta img
+    this.doc.addImage("assets/img1.png", 'PNG', this.right - 260, this.usoPage + 10, 260, 180, 'alias1', 'SLOW');
+    this.doc.text("Figura 5: Después Panorámica", this.right - 260 / 2, this.usoPage + 200, { align: 'center' });
+
+
+  }
+
+  tampleEMT(){
+    this.doc.addImage("assets/images/image2.jpg", 'JPG', 0, 0, 600, 842, 'marca', 'SLOW');
+    this.doc.setTextColor('#D5E1E2')
+    this.doc.setFont("helvetica", "bold");
+    this.doc.setFontSize(20)
+    this.doc.text("REPORTE PUNTOS DE\n" + "SEGUIMIENTO POST TRONADURA", 100, 250, { align: 'left' });
+    this.doc.setTextColor('#DE6719')
+    this.doc.setFontSize(18)
+    this.doc.setFont("helvetica", "normal");
+    this.doc.text("Nombre Faena", 100, 295, { align: 'left' });
+    // #D5E1E2
+    this.doc.setTextColor('#D5E1E2')
+    this.doc.setFontSize(12)
+    this.doc.text("25 de agosto de 2022.", 331, 450, { align: 'left' });
+    // this.doc.addImage("assets/images/image1.png", 'PNG', 20, 23, 217, 70, 'logo', 'SLOW');
+  }
+
+  previsualizar() {
+    var string = this.doc.output('datauristring');
+    var embed = "<embed width='100%' height='100%' src='" + string + "'/>"
+    var x = window.open();
+    x!.document.open();
+    x!.document.write(embed);
+    x!.document.body.style.margin = '0'
+    x!.document.close();
+  }
+
+
+}
