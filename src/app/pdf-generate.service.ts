bdb571b95987820850017435c60d65b105cb44ab
csaave25 <csaavedra@emt.cl>
2023-11-22 19:01:09 -0300

    add fonts

--- src/app/pdf-generate.service.ts
+++ src/app/pdf-generate.service.ts
@@ -1,34 +1,29 @@
 import { Injectable } from '@angular/core';
 import { jsPDF } from 'jspdf'
 import { radares } from './dataPrueba';
-
-
-
-
+import { font, latoRegular } from 'src/assets/fonts/fonts';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PdfGenerateService {
+export class PdfGenerateService  {
 
   constructor() { }
+
 
   rds = radares
 
   startY = 10
-  endY = 835
+  endY = 810
   left = 30
   right = 570
   midPointX = (this.right + this.left) / 2
   midPointY = (this.startY + this.endY) / 2
   totalY = this.endY - this.startY
   numFigura = 1
+  contadorPagina = 1
   usoPage = 0
-
-
   doc = new jsPDF('p', 'pt', 'A4')
-
-
 
   generateNewPage(espacio: number) {
     let zona = ''
@@ -36,7 +31,8 @@ export class PdfGenerateService {
     let norte = 22
     let cota = 22
     let fecha = new Date().toLocaleString()
-    if ((this.totalY - this.usoPage) < espacio) {
+    let espacioUtilizable = (this.totalY - this.usoPage - 20)
+    if (espacioUtilizable < espacio) {
       this.doc.addPage()
       this.generateHeader(zona, este, norte, cota, fecha)
       this.generateFooter()
@@ -44,16 +40,16 @@ export class PdfGenerateService {
   }
 
 
-
   generateHeader(zona: string, este: number, norte: number, cota: number, fecha: string) {
-
+    this.doc.addImage("assets/images/marca.jpg", 'JPG', 0, 0, 600, 842, 'marca2', 'SLOW');
+    this.doc.setTextColor('#013033')
     this.doc.setFontSize(13)
-    this.doc.setFont("helvetica")
     this.doc.text("REPORTE PUNTOS DE SEGUIMIENTO POST TRONADURA", this.midPointX, 40, { align: 'center' });
-    this.doc.setFont("helvetica");
+    this.doc.setFont("helvetica", "normal");
     this.doc.setFontSize(11)
     this.doc.text("TRONADURA " + zona.toUpperCase() + " - COORDENADAS: " + este + " | " + norte + " | " + cota, this.midPointX, 55, { align: 'center' });
     this.doc.text(fecha, this.midPointX, 70, { align: 'center' });
+    this.doc.setFont("helvetica", "bold")
     this.doc.text("2023.12", this.left, 80, { align: 'left' });
     this.doc.text("IYV-RP-VIG-CMZ-04", this.right, 80, { align: 'right' });
     this.doc.line(this.left, 85, this.right, 85);
@@ -62,10 +58,33 @@ export class PdfGenerateService {
   }
 
   generateFooter() {
+    //E-Mining Technology S.A.  •  Calle Limache 3405, Oficina 21, Viña del Mar  •  Teléfono: +56 32 2187440  •  eminingtech.com
+
     this.doc.setFontSize(10)
-    this.doc.text('1', this.midPointX, this.endY, { align: 'center' });
-    this.doc.text("Néstor Ramírez Muena", 30, this.endY, { align: 'left' });
-    this.doc.text("2023.12", 570, this.endY, { align: 'right' });
+    this.doc.setFont('helvetica', 'normal')
+    this.doc.text(this.contadorPagina.toString(), this.midPointX, this.endY - 20, { align: 'center' });
+    this.doc.setFont('helvetica', 'bold')
+    this.doc.text("Néstor Ramírez Muena", 30, this.endY - 20, { align: 'left' });
+    this.doc.text("2023.12", 570, this.endY - 20, { align: 'right' });
+
+    //template emt
+    this.doc.setFontSize(8)
+    let calc = (this.right + this.left) / 6
+    this.doc.setFont("helvetica", 'bold')
+    this.doc.text('E-Mining Technology S.A.', this.left, this.endY, { align: 'left' });
+    this.doc.text('•', this.left + 115, this.endY, { align: 'left' });
+    this.doc.setFont("helvetica", 'normal')
+    this.doc.text('Calle Limache 3405, Oficina 21, Viña del Mar', this.left + 135, this.endY, { align: 'left' });
+    this.doc.setFont("helvetica", 'bold')
+    this.doc.text('•', this.left + 314, this.endY, { align: 'left' });
+    this.doc.setFont("helvetica", 'normal')
+    this.doc.text('Teléfono: +56 32 2187440', this.left + 336, this.endY, { align: 'left' });
+    this.doc.setFont("helvetica", 'bold')
+    this.doc.text('•', this.left + 455, this.endY, { align: 'right' });
+    this.doc.text('eminingtech.com', this.right, this.endY, { align: 'right' });
+
+
+    this.contadorPagina++
   }
 
   planoDeUbicacion() {
@@ -75,8 +94,11 @@ export class PdfGenerateService {
     this.doc.addImage("assets/img1.png", "PNG", this.left + 100, 130, 340, 360, "alias1", 'SLOW');
     this.doc.setFontSize(8)
     this.doc.setFont("helvetica", "normal");
+
+    //aumneto num figura
     this.doc.text("Figura " + this.numFigura + ": Posición de tronadura e instrumentación", this.midPointX, 500, { align: 'center' });
     this.numFigura++
+
     this.usoPage += 430
   }
 
@@ -120,11 +142,15 @@ export class PdfGenerateService {
     this.doc.setFont("helvetica", "normal");
     this.doc.text(nMallas.toString(), calc * 3 + 43, alturaY + 40, { align: 'center' });
     calc = (this.right + this.left) / 3
+    this.doc.setFont("helvetica", "bold");
     this.doc.line(this.left, alturaY + 55, this.right, alturaY + 55);
-    this.doc.text("Volumen (Kton): " , calc, alturaY + 75, { align: 'center' });
-    this.doc.text(volumen.toString(), calc + 20, alturaY + 75, { align: 'center' });
-    this.doc.text("Factor de Carga General: " + factorCarga, calc * 2, alturaY + 75, { align: 'center' });
-    // doc.text(form.factorCarga.value!?.toString(), calc * 2, 715, { align: 'center' });
+    this.doc.text("Volumen (Kton):", calc, alturaY + 75, { align: 'center' });
+    this.doc.setFont("helvetica", "normal");
+    this.doc.text(volumen.toString(), calc + 50, alturaY + 75, { align: 'center' });
+    this.doc.setFont("helvetica", "bold");
+    this.doc.text("Factor de Carga General: ", calc * 2, alturaY + 75, { align: 'center' });
+    this.doc.setFont("helvetica", "normal");
+    this.doc.text(factorCarga.toString(), calc * 2 + 70, alturaY + 75, { align: 'center' });
 
     this.usoPage = alturaY + 95
 
@@ -143,7 +169,12 @@ export class PdfGenerateService {
     this.doc.addImage("assets/img/mapa1.png", 'PNG', midIMG, this.usoPage + 15, 320, 220, 'figura2', 'SLOW');
     this.doc.setFontSize(8)
     this.doc.setFont("helvetica", "normal");
-    this.doc.text("Figura 2: Mapa sintético de desplazamientos Vs. tiempo", this.midPointX, this.usoPage + 245, { align: 'center' });
+
+
+    this.doc.text("Figura " + this.numFigura + ": Mapa sintético de desplazamientos Vs. tiempo", this.midPointX, this.usoPage + 245, { align: 'center' });
+    //Aumento num figura
+    this.numFigura++
+
     this.usoPage += 230
 
     //2da Img
@@ -151,7 +182,11 @@ export class PdfGenerateService {
     this.doc.addImage("../../assets/img/mapa2.png", 'PNG', midIMG, this.usoPage + 25, 320, 220, 'figura 3', 'SLOW');
     this.doc.setFontSize(8)
     this.doc.setFont("helvetica", "normal");
-    this.doc.text("Figura 3: Gráfico de desplazamientos Vs. tiempo", this.midPointX, this.usoPage + 255, { align: 'center' });
+
+    this.doc.text("Figura " + this.numFigura + ": Gráfico de desplazamientos Vs. tiempo", this.midPointX, this.usoPage + 255, { align: 'center' });
+    //Aumento num figura
+    this.numFigura++
+
     this.usoPage += 230
 
 
@@ -212,70 +247,111 @@ export class PdfGenerateService {
     this.doc.setFont("helvetica", "normal");
     //Primera img
     this.doc.addImage("assets/img1.png", 'PNG', this.left, this.usoPage + 10, 260, 180, 'alias1', 'SLOW');
-    this.doc.text("Figura 4: Antes Panorámica ", 260 / 2 + this.left, this.usoPage + 200, { align: 'center' });
+    this.doc.text("Figura " + this.numFigura + ": Antes Panorámica ", 260 / 2 + this.left, this.usoPage + 200, { align: 'center' });
+    //Aumento num figura
+    this.numFigura++
 
     //segunda img
     this.doc.addImage("assets/img1.png", 'PNG', this.right - 260, this.usoPage + 10, 260, 180, 'alias1', 'SLOW');
-    this.doc.text("Figura 5: Después Panorámica", this.right - 260 / 2, this.usoPage + 200, { align: 'center' });
-
+    this.doc.text("Figura " + this.numFigura + ": Después Panorámica", this.right - 260 / 2, this.usoPage + 200, { align: 'center' });
+    //Aumento num figura
+    this.numFigura++
 
     this.usoPage += 210
     this.generateNewPage(260)
 
     //3ra img
     this.doc.addImage("assets/img1.png", 'PNG', this.left, this.usoPage + 10, 260, 180, 'alias1', 'SLOW');
-    this.doc.text("Figura 4: Antes Panorámica ", 260 / 2 + this.left, this.usoPage + 200, { align: 'center' });
+    this.doc.text("Figura " + this.numFigura + ": Antes Panorámica ", 260 / 2 + this.left, this.usoPage + 200, { align: 'center' });
+    //Aumento num figura
+    this.numFigura++
 
     //4ta img
     this.doc.addImage("assets/img1.png", 'PNG', this.right - 260, this.usoPage + 10, 260, 180, 'alias1', 'SLOW');
-    this.doc.text("Figura 5: Después Panorámica", this.right - 260 / 2, this.usoPage + 200, { align: 'center' });
+    this.doc.text("Figura " + this.numFigura + ": Después Panorámica", this.right - 260 / 2, this.usoPage + 200, { align: 'center' });
+    //Aumento num figura
+    this.numFigura++
 
     this.usoPage += 210
     this.generateNewPage(260)
 
     //5ra img
     this.doc.addImage("assets/img1.png", 'PNG', this.left, this.usoPage + 10, 260, 180, 'alias1', 'SLOW');
-    this.doc.text("Figura 4: Antes Panorámica ", 260 / 2 + this.left, this.usoPage + 200, { align: 'center' });
+    this.doc.text("Figura " + this.numFigura + ": Antes Panorámica ", 260 / 2 + this.left, this.usoPage + 200, { align: 'center' });
+    //Aumento num figura
+    this.numFigura++
 
     //6ta img
     this.doc.addImage("assets/img1.png", 'PNG', this.right - 260, this.usoPage + 10, 260, 180, 'alias1', 'SLOW');
-    this.doc.text("Figura 5: Después Panorámica", this.right - 260 / 2, this.usoPage + 200, { align: 'center' });
+    this.doc.text("Figura " + this.numFigura + ": Después Panorámica", this.right - 260 / 2, this.usoPage + 200, { align: 'center' });
+    //Aumento num figura
+    this.numFigura++
 
     this.usoPage += 210
     this.generateNewPage(260)
 
     //7ra img
     this.doc.addImage("assets/img1.png", 'PNG', this.left, this.usoPage + 10, 260, 180, 'alias1', 'SLOW');
-    this.doc.text("Figura 4: Antes Panorámica ", 260 / 2 + this.left, this.usoPage + 200, { align: 'center' });
+    this.doc.text("Figura  " + this.numFigura + ": Antes Panorámica ", 260 / 2 + this.left, this.usoPage + 200, { align: 'center' });
+    //Aumento num figura
+    this.numFigura++
 
     //8ta img
     this.doc.addImage("assets/img1.png", 'PNG', this.right - 260, this.usoPage + 10, 260, 180, 'alias1', 'SLOW');
-    this.doc.text("Figura 5: Después Panorámica", this.right - 260 / 2, this.usoPage + 200, { align: 'center' });
+    this.doc.text("Figura " + this.numFigura + ": Después Panorámica", this.right - 260 / 2, this.usoPage + 200, { align: 'center' });
+    //Aumento num figura
+    this.numFigura++
 
 
   }
 
-  tampleEMT(){
+  tampleEMT() {
+    this.implementarFuentes()
+
     this.doc.addImage("assets/images/image2.jpg", 'JPG', 0, 0, 600, 842, 'marca', 'SLOW');
     this.doc.setTextColor('#D5E1E2')
-    this.doc.setFont("helvetica", "bold");
+    this.doc.setFont("Lato", "normal");
     this.doc.setFontSize(20)
     this.doc.text("REPORTE PUNTOS DE\n" + "SEGUIMIENTO POST TRONADURA", 100, 250, { align: 'left' });
     this.doc.setTextColor('#DE6719')
     this.doc.setFontSize(18)
-    this.doc.setFont("helvetica", "normal");
+    this.doc.setFont("Lato", "bold");
     this.doc.text("Nombre Faena", 100, 295, { align: 'left' });
-    // #D5E1E2
     this.doc.setTextColor('#D5E1E2')
     this.doc.setFontSize(12)
     this.doc.text("25 de agosto de 2022.", 331, 450, { align: 'left' });
     // this.doc.addImage("assets/images/image1.png", 'PNG', 20, 23, 217, 70, 'logo', 'SLOW');
+
+
+    this.doc.setFontSize(8)
+    this.doc.setTextColor("#D5E1E2")
+    let calc = (this.right + this.left) / 6
+    this.doc.setFont("helvetica", 'bold')
+    this.doc.text('E-Mining Technology S.A.', this.left, this.endY, { align: 'left' });
+    this.doc.text('•', this.left + 115, this.endY, { align: 'left' });
+    this.doc.setFont("helvetica", 'normal')
+    this.doc.text('Calle Limache 3405, Oficina 21, Viña del Mar', this.left + 135, this.endY, { align: 'left' });
+    this.doc.setFont("helvetica", 'bold')
+    this.doc.text('•', this.left + 314, this.endY, { align: 'left' });
+    this.doc.setFont("helvetica", 'normal')
+    this.doc.text('Teléfono: +56 32 2187440', this.left + 336, this.endY, { align: 'left' });
+    this.doc.setFont("helvetica", 'bold')
+    this.doc.text('•', this.left + 455, this.endY, { align: 'right' });
+    this.doc.text('eminingtech.com', this.right, this.endY, { align: 'right' });
+  }
+
+
+  implementarFuentes() {
+    this.doc.addFileToVFS("Lato-Font-bold.ttf", font);
+    this.doc.addFont("Lato-Font-bold.ttf", "Lato", "bold");
+    this.doc.addFileToVFS("Lato-Font-normal.ttf", latoRegular);
+    this.doc.addFont("Lato-Font-normal.ttf", "Lato", "normal");
   }
 
   previsualizar() {
-    var string = this.doc.output('datauristring');
-    var embed = "<embed width='100%' height='100%' src='" + string + "'/>"
-    var x = window.open();
+    let string = this.doc.output('datauristring');
+    let embed = "<embed width='100%' height='100%' src='" + string + "'/>"
+    let x = window.open();
     x!.document.open();
     x!.document.write(embed);
     x!.document.body.style.margin = '0'
@@ -284,3 +360,5 @@ export class PdfGenerateService {
 
 
 }
+
+
