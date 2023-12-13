a36c490973910a1397b63524ba65bb534255a261
csaave25 <csaavedra@emt.cl>
2023-12-13 19:27:00 -0300

    informe-efe 0.3

--- src/app/informe-efe/data.ts
+++ src/app/informe-efe/data.ts
@@ -12,45 +12,17 @@ export const colores = {
     amarillo: '#C2C71B'
 }
 
-
-export const data = {
-    ano: 2023,
-    mes: 11,
-    numInfo: 5,
-    titulo: 'INFORME MENSUAL N°',
-    subTiutlo: 'ANÁLISIS DE TENDENCIAS DE COMPORTAMIENTO DE LA INFRAESTRUCTURA',
-    subTitulo2: 'SERVICIO DE MONITOREO GEOTÉCNICO TALUD DE ACCESO PUENTE LAS CUCHARAS DE METRO VALPARAÍSO',
-    tablaResumen: {
-        elaborado: 'ANDRÉS LEÓN',
-        cargo: 'INGENIERO GEOTÉCNICO',
-        revisado: 'LEONARDO ZAHR',
-        cargo2: 'LÍDER DE GEOTECNIA',
-        aprobado: 'LEONARDO ZAHR',
-        cargo3: 'ADMINISTRADOR DE CONTRATO'
-    },
-    seccion1: {
-        estadogeneral: 'La plataforma posee comunicación con piezómetros, Geo Centinelas de Corte y Deformación (GCC y GCC). Además de tener cargadas la información de desplazamientos de los prismas del sector de interés.\nDesde la ultima revisión de información han ocurrido dos eventos que gatillan el modo vigilancia el 29 de Octubre y 10 de Noviembre.\nEl comportamiento del Talud de Acceso a Puente Las Cucharas se mantiene estable, con todas las estaciones en estado Normal.\nDesde el 21 de Octubre hasta el 21 de Noviembre de 2023 no se superaron los umbrales establecidos, emitiéndose un reporte post sismo el 6 de Noviembre (5.1 Mw).',
-        observaciones: {
-            piezometro: 'No se observan variaciones significativas a lo largo del mes.',
-            gcd: 'Vegetación suele tapar los pequeños paneles de los equipos haciendo que los equipos se descarguen.',
-            gcc: 'Sin corte hasta el 15/11/2023.',
-            prisma: 'Prismas 04, 05, 06 y 09 sin lectura debido a que la vegetación obstaculiza la toma de datos.\nPrisma 21 con lecturas no representativas del talud debido a que el prisma no se encuentra fijo.'
-        }
-    }
-}
-
 export const obtenerAncho = (pdfGen: jsPDF, texto: string, margen: number) => {
     let dimensiones = pdfGen.getTextDimensions(texto)
     let tamanoFuente = pdfGen.getFontSize()
     let lines = dimensiones.w / margen
-    let valor = Math.trunc(lines) + 1.7
+    let valor = Math.trunc(lines) + 1.7 // Creo que 1.7 (espaciado) se multiplica por la cantidad de lineas
     return tamanoFuente * valor
 }
 
 export const dividirTexto = (text: string) => {
     return text.split('\n')
 }
-
 
 export function justify(pdfGen: jsPDF, text: string, xStart: number, yStart: number, textWidth: number) {
     text = text.replace(/(?:\r\n|\r|\n)/g, ' ');
@@ -96,15 +68,87 @@ interface IWordInfo {
     wordLength: number;
 }
 
-
-export const espaciarTextosLargos = (doc: jsPDF, texto: string, start: number, initX: number, finishX: number) => {
+export const espaciarTextosLargos = (doc: jsPDF, texto: string, margenTop: number, inicio: number, anchoMax: number) => {
     const arrText = dividirTexto(texto)
     if (arrText) {
         arrText.forEach(str => {
-            justify(doc, str, initX, start, finishX)
-            start = start + obtenerAncho(doc, str, finishX)
+            justify(doc, str, inicio, margenTop, anchoMax)
+            margenTop = margenTop + obtenerAncho(doc, str, anchoMax)
         })
     }
 
-    return start
+    return margenTop
 }
+
+
+
+export const data = {
+    ano: 2023,
+    mes: 11,
+    numInfo: 5,
+    titulo: 'INFORME MENSUAL N°',
+    subTiutlo: 'ANÁLISIS DE TENDENCIAS DE COMPORTAMIENTO DE LA INFRAESTRUCTURA',
+    subTitulo2: 'SERVICIO DE MONITOREO GEOTÉCNICO TALUD DE ACCESO PUENTE LAS CUCHARAS DE METRO VALPARAÍSO',
+    tablaResumen: {
+        elaborado: 'ANDRÉS LEÓN',
+        cargo: 'INGENIERO GEOTÉCNICO',
+        revisado: 'LEONARDO ZAHR',
+        cargo2: 'LÍDER DE GEOTECNIA',
+        aprobado: 'LEONARDO ZAHR',
+        cargo3: 'ADMINISTRADOR DE CONTRATO'
+    },
+    seccion1: {
+        estadoIntrumentos: {
+            piezometro: true,
+            gcd: true,
+            gcc: false,
+            prisma: false
+        },
+        estadogeneral: 'La plataforma posee comunicación con piezómetros, Geo Centinelas de Corte y Deformación (GCC y GCC). Además de tener cargadas la información de desplazamientos de los prismas del sector de interés.\nDesde la ultima revisión de información han ocurrido dos eventos que gatillan el modo vigilancia el 29 de Octubre y 10 de Noviembre.\nEl comportamiento del Talud de Acceso a Puente Las Cucharas se mantiene estable, con todas las estaciones en estado Normal.\nDesde el 21 de Octubre hasta el 21 de Noviembre de 2023 no se superaron los umbrales establecidos, emitiéndose un reporte post sismo el 6 de Noviembre (5.1 Mw).',
+        observaciones: {
+            piezometro: 'No se observan variaciones significativas a lo largo del mes.',
+            gcd: 'Vegetación suele tapar los pequeños paneles de los equipos haciendo que los equipos se descarguen.',
+            gcc: 'Sin corte hasta el 15/11/2023.',
+            prisma: 'Prismas 04, 05, 06 y 09 sin lectura debido a que la vegetación obstaculiza la toma de datos.\nPrisma 21 con lecturas no representativas del talud debido a que el prisma no se encuentra fijo.'
+        },
+        tablaMonitoreo: {
+            alarmas: 0,
+            alertas: 0,
+            vigilacia: 2,
+            sismo: 1
+        },
+    },
+    seccionAnalisis: [
+        {
+            obsGenerales: 'Durante el periodo comprendido entre el 21 de Octubre de 2023 al 21 de Noviembre la estación se encontró en estado Normal.',
+            obsEspecificas: {
+                obsGrafico1: '',
+                obsGrafico2: '',
+                gcd: 'Tendencias estables, últimos datos del 15 de Noviembre de 2023',
+                gcc: 'Sin cortes hasta el último dato 15 de Noviembre de 2023.'
+            },
+            obsPiezometro: 'No se observan variaciones significativas'
+        },
+        {
+            obsGenerales: 'Durante el periodo comprendido entre el 21 de Octubre de 2023 al 21 de Noviembre la estación se encontró en estado NORMAL.',
+            obsEspecificas: {
+                obsGrafico1: '',
+                obsGrafico2: 'Dada la sensibilidad del sensor, se puede producir cambios bruscos en las lecturas debido a reacomodos puntuales de material producto de la perforación misma o por precipitaciones.\nSi bien se observan cambios bruscos no existe en general una tendencia que apunte hacia un fallamiento.\nLas ventanas en gris muestran periodos en donde los nodos se descargaron y no registraron datos.',
+                gcd: 'Tendencias estables*. Últimos datos del 15/10123.',
+                gcc: 'Sin cortes hasta el último dato 15 de Noviembre de 2023.'
+            },
+            obsPiezometro: 'No se observan variaciones significativas'
+        },
+        {
+            obsGenerales: 'Durante el periodo comprendido entre el 21 de Octubre de 2023 al 21 de Noviembre la estación se encontró en estado NORMAL.',
+            obsEspecificas: {
+                obsGrafico1: '',
+                obsGrafico2: 'Se observa un canal saturado a 2,9m de profundidad, pero que no se correlaciona con la deformación que muestran los demás canales.',
+                gcd: 'Tendencias estables*. Últimos datos del 15/10123.',
+                gcc: 'Sin cortes hasta el último dato 15 de Noviembre de 2023.'
+            },
+            obsPiezometro: 'No se observan variaciones significativas'
+        },
+    ]
+}
+
--- src/app/informe-efe/informe.service.ts
+++ src/app/informe-efe/informe.service.ts
@@ -37,9 +37,8 @@ export class InformeService {
     this.doc.text('Estado de la Instrumentación', this.margenDer - 165, this.cominezoContenidoY + 10, { align: 'left', maxWidth: this.maxMargen });
 
     this.doc.setFont("Lato", "normal");
-    let flag = true
 
-    if (flag) {
+    if (data.seccion1.estadoIntrumentos.piezometro) {
       this.doc.setTextColor(colores.verde)
       this.doc.addImage("assets/EFE/checkmark.png", 'PNG', this.margenDer - 165, this.cominezoContenidoY + 20, 12, 12, 'check' + this.contadorPagina, 'SLOW');
     } else {
@@ -49,7 +48,7 @@ export class InformeService {
     this.doc.text('Piezómetros', this.margenDer - 150, this.cominezoContenidoY + 30, { align: 'left', maxWidth: this.maxMargen });
 
 
-    if (flag) {
+    if (data.seccion1.estadoIntrumentos.gcd) {
       this.doc.setTextColor(colores.verde)
       this.doc.addImage("assets/EFE/checkmark.png", 'PNG', this.margenDer - 165, this.cominezoContenidoY + 40, 12, 12, 'check' + this.contadorPagina, 'SLOW');
     } else {
@@ -60,7 +59,7 @@ export class InformeService {
 
 
 
-    if (flag) {
+    if (data.seccion1.estadoIntrumentos.gcc) {
       this.doc.setTextColor(colores.verde)
       this.doc.addImage("assets/EFE/checkmark.png", 'PNG', this.margenDer - 165, this.cominezoContenidoY + 60, 12, 12, 'check' + this.contadorPagina, 'SLOW');
     } else {
@@ -70,8 +69,8 @@ export class InformeService {
 
     this.doc.text('GCC: Geo Centinela Corte', this.margenDer - 150, this.cominezoContenidoY + 70, { align: 'left', maxWidth: this.maxMargen });
 
-    flag = false
-    if (flag) {
+
+    if (data.seccion1.estadoIntrumentos.prisma) {
       this.doc.setTextColor(colores.verde)
       this.doc.addImage("assets/EFE/checkmark.png", 'PNG', this.margenDer - 165, this.cominezoContenidoY + 80, 12, 12, 'check' + this.contadorPagina, 'SLOW');
 
@@ -121,7 +120,7 @@ export class InformeService {
 
     this.doc.setFillColor(237, 243, 38);
     this.doc.circle(this.margenDer - 70, this.cominezoContenidoY + 165, 7, "FD");
-    this.doc.text('Precaución', this.margenDer - 57, this.cominezoContenidoY + 169, { align: 'left', maxWidth: this.maxMargen });
+    this.doc.text('Precaución', this.margenDer - 59, this.cominezoContenidoY + 169, { align: 'left', maxWidth: this.maxMargen });
 
     this.doc.setFillColor(238, 166, 33);
     this.doc.circle(this.margenDer - 155, this.cominezoContenidoY + 195, 7, "FD");
@@ -129,7 +128,7 @@ export class InformeService {
 
     this.doc.setFillColor(238, 42, 33);
     this.doc.circle(this.margenDer - 70, this.cominezoContenidoY + 195, 7, "FD");
-    this.doc.text('Alarma', this.margenDer - 57, this.cominezoContenidoY + 200, { align: 'left', maxWidth: this.maxMargen });
+    this.doc.text('Alarma', this.margenDer - 59, this.cominezoContenidoY + 200, { align: 'left', maxWidth: this.maxMargen });
 
 
     //Estado General
@@ -146,18 +145,183 @@ export class InformeService {
     this.doc.setFontSize(10)
     this.doc.setFont("Lato", "bold");
     this.doc.text('Observaciones:', this.margenIzq, this.usoPagina + 10, { align: 'left', maxWidth: this.maxMargen });
-
-
+    this.usoPagina += 20
+    let page = 1
     autoTable(this.doc, {
       // theme: 'grid',
-      styles: { halign: 'left',font: 'Lato', fontStyle: 'normal', fontSize: 10, fillColor: undefined, lineColor: [1, 48, 51], textColor: [1, 48, 51]},
+      styles: { halign: 'left', font: 'Lato', fontStyle: 'normal', fontSize: 10, fillColor: undefined, lineColor: [1, 48, 51], textColor: [1, 48, 51] },
       headStyles: { font: 'Lato', fontStyle: 'bold', fillColor: undefined, lineWidth: .1 },
       body: [['Piezómetros:', data.seccion1.observaciones.piezometro], ['GCD:', data.seccion1.observaciones.gcd], ['GCC:', data.seccion1.observaciones.gcc], ['Prismas:', data.seccion1.observaciones.prisma]],
-      margin: { left:this.margenIzq},
-      startY: this.usoPagina + 20,
-      columnStyles: {0:{cellWidth: 80}, 1: {halign: 'justify'}},
-      alternateRowStyles: {fillColor: undefined}
+      margin: { left: this.margenIzq, top: this.cominezoContenidoY },
+      startY: this.usoPagina,
+      columnStyles: { 0: { cellWidth: 80 }, 1: { halign: 'justify' } },
+      alternateRowStyles: { fillColor: undefined },
+      rowPageBreak: 'avoid',
+      didDrawCell: (data) => {
+        if (data.column.index == 1 && data.cell.section == 'body') {
+          this.usoPagina += data.row.height
+        }
+      },
+
+      didDrawPage: (data) => {
+        if (data.pageNumber != page) {
+          this.usoPagina = this.cominezoContenidoY
+          page = data.pageNumber
+        }
+      },
+
+      willDrawPage: (data) => {
+
+        if (page != data.pageNumber) {
+          this.doc.addImage("assets/images/marca.jpg", 'JPG', 0, 0, 612, 792, 'marca-x', 'SLOW');
+          this.implementarHeader()
+          this.implementarFooter()
+        }
+      },
     })
+
+    this.usoPagina += 20
+
+    this.crearNuevaPagina(this.usoPagina + 89)
+
+    //Tabla de monitoreo geotecnico
+    this.doc.setFontSize(10)
+    this.doc.setFont("Lato", "bold");
+    this.doc.text('Monitoreo Geotécnico', this.puntoMedio, this.usoPagina, { align: 'center', maxWidth: this.maxMargen });
+
+    this.usoPagina += 10
+
+    autoTable(this.doc, {
+      theme: 'grid',
+      styles: { halign: 'left', font: 'Lato', fontStyle: 'normal', fontSize: 8, fillColor: undefined, lineColor: [1, 48, 51], textColor: [1, 48, 51] },
+      headStyles: { font: 'Lato', fontStyle: 'bold', fillColor: undefined, lineWidth: .1 },
+      body: [['Alarmas mes', data.seccion1.tablaMonitoreo.alarmas], ['Alerta mes', data.seccion1.tablaMonitoreo.alertas], ['Modo Vigilancia', data.seccion1.tablaMonitoreo.vigilacia], ['Reporte post sismo ', data.seccion1.tablaMonitoreo.sismo]],
+      margin: { left: this.puntoMedio - (70 + 90) / 2, top: this.usoPagina },
+      startY: this.usoPagina,
+      columnStyles: { 0: { halign: 'left', cellWidth: 90 }, 1: { halign: 'center', cellWidth: 70 } },
+      alternateRowStyles: { fillColor: undefined },
+      rowPageBreak: 'avoid',
+
+      didDrawCell: (data) => {
+        let width = data.cell.width
+        let height = data.cell.height
+        let x = data.cell.x + 5
+        let y = data.cell.y + 3
+        if (data.column.index == 0 && data.row.index == 0) {
+
+          this.doc.setFillColor(238, 42, 33);
+          this.doc.circle(x + width - 20, y + 7, 4, "FD");
+
+        }
+        if (data.column.index == 0 && data.row.index == 1) {
+          this.doc.setFillColor(238, 166, 33);
+          this.doc.circle(x + width - 20, y + 7, 4, "FD");
+        }
+      }
+    })
+  }
+
+  generarAnalisisDeDatos() {
+    let nEstacion = 6
+    data.seccionAnalisis.forEach(seccion => {
+      this.crearNuevaPagina(this.usoPagina + 400)
+
+      this.doc.setFontSize(10)
+      this.doc.setFont("Lato", "bold");
+      this.doc.text('Análisis de datos estación ' + nEstacion, this.margenIzq, this.usoPagina, { align: 'left', maxWidth: this.maxMargen });
+      this.usoPagina += 10
+
+      this.doc.addImage("assets/EFE/Luis.jpg", 'JPG', this.margenIzq, this.usoPagina, 320, 220, 'LUIS' + this.contadorPagina, 'SLOW');
+
+      //Tabla de criterios
+      this.doc.setTextColor(colores.negro)
+      this.doc.setFontSize(12)
+      this.doc.setFont("Lato", "bold");
+      this.doc.text('Criterios de Aceptabilidad', this.margenDer - 215, this.usoPagina + 10, { align: 'left', maxWidth: this.maxMargen });
+      this.usoPagina += 10
+
+      this.doc.setFont("Lato", "normal");
+
+      this.doc.setLineWidth(0.1);
+      this.doc.setFillColor(89, 238, 33);
+      this.doc.circle(this.margenDer - 205, this.usoPagina + 24, 7, "FD");
+      this.doc.text('Normal', this.margenDer - 194, this.usoPagina + 28, { align: 'left', maxWidth: this.maxMargen });
+
+      this.doc.setFillColor(237, 243, 38);
+      this.doc.circle(this.margenDer - 123, this.usoPagina + 24, 7, "FD");
+      this.doc.text('Precaución', this.margenDer - 112, this.usoPagina + 28, { align: 'left', maxWidth: this.maxMargen });
+      this.usoPagina += 30
+
+      this.doc.setFillColor(238, 166, 33);
+      this.doc.circle(this.margenDer - 205, this.usoPagina + 24, 7, "FD");
+      this.doc.text('Alerta', this.margenDer - 194, this.usoPagina + 28, { align: 'left', maxWidth: this.maxMargen });
+
+      this.doc.setFillColor(238, 42, 33);
+      this.doc.circle(this.margenDer - 123, this.usoPagina + 24, 7, "FD");
+      this.doc.text('Alarma', this.margenDer - 112, this.usoPagina + 28, { align: 'left', maxWidth: this.maxMargen });
+      this.usoPagina += 28
+
+      this.doc.setTextColor(colores.negro)
+      this.doc.setFontSize(10)
+      this.doc.setFont("Lato", "bold");
+      this.doc.text('Observaciones generales', this.margenDer - 215, this.usoPagina + 30, { align: 'left', maxWidth: this.maxMargen });
+      this.usoPagina += 30
+
+      this.doc.setFontSize(10)
+      this.doc.setFont("Lato", "normal");
+      this.usoPagina += 20 + espaciarTextosLargos(this.doc, seccion.obsGenerales, this.usoPagina + 20, this.margenDer - 215, 215 + this.margenIzq)
+
+      //Estado mensual sensores
+      this.usoPagina = this.cominezoContenidoY + 230
+      this.doc.setFontSize(10)
+      this.doc.setFont("Lato", "bold");
+      this.doc.text('Estado mensual sensores GCD y GCC', this.margenIzq, this.usoPagina + 30, { align: 'left', maxWidth: this.maxMargen });
+      this.usoPagina += 20
+
+      this.doc.addImage("assets/EFE/Luis.jpg", 'JPG', this.margenIzq, this.usoPagina + 20, 230, 220, 'LUIS' + this.contadorPagina, 'SLOW');
+      this.doc.addImage("assets/EFE/Luis.jpg", 'JPG', this.margenDer - 320 + this.margenIzq, this.usoPagina + 20, 320, 220, 'LUIS' + this.contadorPagina, 'SLOW');
+      this.usoPagina += 20 + 220
+
+
+
+      let texto1 = obtenerAncho(this.doc, seccion.obsEspecificas.gcd, 188)
+      let texto2 = obtenerAncho(this.doc, seccion.obsEspecificas.gcc, 188)
+      if (texto1 > texto2) {
+        this.crearNuevaPagina(this.usoPagina + texto1 + 50)
+        texto2 = texto1
+      }
+      if (texto2 > texto1) {
+        this.crearNuevaPagina(this.usoPagina + texto2 + 50)
+        texto1 = texto2
+      }
+
+      this.doc.setFontSize(10)
+      this.doc.setFont("Lato", "bold");
+      this.doc.text('Observaciones específicas GCC y GCD', this.margenIzq, this.usoPagina + 30, { align: 'left', maxWidth: this.maxMargen });
+      this.usoPagina += 30
+
+      this.doc.setFontSize(10)
+      this.doc.setFont("Lato", "bold");
+      this.doc.text('GCD Pozo 13: ', this.margenIzq + 30, this.usoPagina + 20, { align: 'left', maxWidth: this.maxMargen });
+      this.doc.text('GCC Pozo 13: ', this.margenIzq + 310, this.usoPagina + 20, { align: 'left', maxWidth: this.maxMargen });
+
+      this.doc.setFont("Lato", "normal");
+      espaciarTextosLargos(this.doc, seccion.obsEspecificas.gcd, this.usoPagina + 20, this.margenIzq + 95, 188)
+      espaciarTextosLargos(this.doc, seccion.obsEspecificas.gcc, this.usoPagina + 20, this.margenIzq + 375, 188)
+      this.usoPagina += texto1 + 30
+    
+      
+      this.crearNuevaPagina(this.usoPagina + obtenerAncho(this.doc, seccion.obsEspecificas.obsGrafico2, this.margenDer - this.margenIzq - 8 ) + 2  )
+      this.doc.setFontSize(10)
+      this.doc.setFont("Lato", "normal");
+      espaciarTextosLargos(this.doc, seccion.obsEspecificas.obsGrafico2, this.usoPagina, this.margenIzq + 30, this.margenDer - this.margenIzq - 8)
+
+
+      this.usoPagina += 250
+      nEstacion++
+    })
+
+
   }
 
   generarTablaResumen() {
@@ -171,11 +335,11 @@ export class InformeService {
     let margenIzq = this.margenIzq + 72
     this.doc.setTextColor(colores.azul)
     this.doc.setFont("Montserrat", "bold");
-    this.doc.setFontSize(25)
+    this.doc.setFontSize(22)
     this.doc.text(data.titulo + data.numInfo, this.puntoMedio, altura, { align: 'center' });
 
     this.doc.setTextColor(colores.negro)
-    this.doc.setFontSize(18)
+    this.doc.setFontSize(16)
     this.doc.setFont("Lato", "normal");
     this.doc.text(data.subTiutlo, this.puntoMedio, altura + 30, { align: 'center', maxWidth: this.maxMargen });
 
@@ -236,11 +400,11 @@ export class InformeService {
     this.doc.addImage("assets/images/image2.jpg", 'JPG', 0, 0, 612, 792, 'marca-1', 'SLOW');
     this.doc.setTextColor(colores.blanco)
     this.doc.setFont("Montserrat", "bold");
-    this.doc.setFontSize(25)
+    this.doc.setFontSize(22)
     this.doc.text(data.titulo + data.numInfo, this.puntoMedio, altura, { align: 'center' });
 
     this.doc.setTextColor(colores.blanco)
-    this.doc.setFontSize(18)
+    this.doc.setFontSize(16)
     this.doc.setFont("Lato", "normal");
     this.doc.text(data.subTiutlo, this.puntoMedio, altura + 30, { align: 'center', maxWidth: this.maxMargen });
     altura += obtenerAncho(this.doc, data.subTiutlo, this.maxMargen);
@@ -282,7 +446,7 @@ export class InformeService {
     this.doc.setFontSize(11)
     let mes = this.mes
     mes = mes.charAt(0).toUpperCase() + mes.slice(1);
-    this.doc.text(mes + ' ' + data.ano, this.puntoMedio, altura + 28, { align: 'center' })
+    this.doc.text(mes + ' ' + data.ano, this.puntoMedio, altura + 20, { align: 'center' })
   }
 
   implementarFuentes() {
@@ -306,11 +470,18 @@ export class InformeService {
     this.implementarFooter()
   }
 
+  crearNuevaPagina(uso: number) {
+    if (uso + 10 > this.finalPagina - 15) {
+      this.nuevaPagina()
+    }
+  }
+
   crearInforme() {
     this.implementarFuentes()
     this.implementarPortada()
     this.generarTablaResumen()
     this.generarSeccion1()
+    this.generarAnalisisDeDatos()
   }
 
   generarInforme() {
