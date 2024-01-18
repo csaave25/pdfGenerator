64b17e11e183b5f3dcdfb2a50eeb1c9e367049e5
csaave25 <csaavedra@emt.cl>
2024-01-18 17:07:01 -0300

    informe de sismos v0.1

--- src/app/reportes/reporte-sismo/api.service.ts
+++ src/app/reportes/reporte-sismo/api.service.ts
@@ -6,11 +6,26 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
 
-  constructor(private http : HttpClient) { }
+  constructor(private http: HttpClient) { }
 
 
-  getUltimosSismosChile(){
+  getUltimosSismosChile() {
     return this.http.get('https://api.gael.cloud/general/public/sismos')
+  }
+
+  getSismosUSGC(fecha: string, latitud: number, longitud: number) {
+    let siguienteDia = new Date(fecha)
+    siguienteDia.setDate(siguienteDia.getDate() + 1)
+    let manana = new Date(siguienteDia).toISOString().slice(0,10)
+    
+    return this.http.get(`https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minmagnitude=4&latitude=${latitud}&longitude=${longitud}&maxradiuskm=450&starttime=${fecha}&endtime=${manana}`)
+  }
+
+  getMoreInfo(id : string){
+    // https://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us7000labe&format=geojson
+    return this.http.get(`https://earthquake.usgs.gov/fdsnws/event/1/query?eventid=${id}&format=geojson`)
+
+
   }
 
 }
--- src/app/reportes/reporte-sismo/reporte-sismos/reporte-sismos.component.ts
+++ src/app/reportes/reporte-sismo/reporte-sismos/reporte-sismos.component.ts
@@ -1,20 +1,88 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import * as leaf from 'leaflet';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reporte-sismos',
   templateUrl: './reporte-sismos.component.html',
   styleUrls: ['./reporte-sismos.component.scss']
 })
-export class ReporteSismosComponent implements OnInit {
+export class ReporteSismosComponent implements OnInit, AfterViewInit {
 
   constructor(private api: ApiService) { }
 
   dataUltimosSismos: any[] = []
-
+  map: any
+  idSismoSeleccionado: string = 'seleccione un sismo'
+  marker: any
+  inputs = new FormGroup({
+    fecha: new FormControl('')
+  })
+  fechaMaxima = new Date().toISOString().slice(0, 10)
 
   ngOnInit(): void {
-    this.loadUltimosSismosChile()
+    // this.loadUltimosSismosChile()
+  }
+  ngAfterViewInit(): void {
+    this.loadMapa()
+  }
+
+  onSeleccionarFecha() {
+    this.dataUltimosSismos = []
+    let fecha = this.inputs.get('fecha')?.value
+    if (this.marker) {
+      this.map.removeLayer(this.marker)
+    }
+    if (fecha)
+      this.loadSismoPorFecha(fecha, -31.7172237, -70.4905051)
+  }
+
+  seleccionarSismo(id: string, event: Event) {
+    event.preventDefault()
+    this.idSismoSeleccionado = id
+    var estrella = leaf.icon({
+      iconUrl: './assets/Sismos/sismoMarker.png',
+      iconSize: [30, 30], // size of the icon
+      // iconAnchor: [20, 20], // point of the icon which will correspond to marker's location
+      popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+
+    let dato = this.dataUltimosSismos.filter((data: any) => data.id == id)[0]
+    if (dato) {
+      if (this.marker) {
+        this.map.removeLayer(this.marker)
+      }
+      this.marker = leaf.marker([dato.latitud, dato.longitud], { icon: estrella }).addTo(this.map);
+    }
+  }
+
+  loadMapa() {
+    this.map = leaf.map('map', {
+      center: [-31.7172237, -70.4905051],
+      zoom: 6,
+      zoomControl: false,
+      scrollWheelZoom: false
+    });
+
+
+    var estrella = leaf.icon({
+      iconUrl: './assets/Sismos/minaMarker.png',
+      iconSize: [20, 20], // size of the icon
+      popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+
+
+
+    leaf.marker([-31.7172237, -70.4905051], { icon: estrella }).addTo(this.map);
+
+    const tiles = leaf.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
   }
 
   loadUltimosSismosChile() {
@@ -23,5 +91,32 @@ export class ReporteSismosComponent implements OnInit {
     })
   }
 
+  loadSismoPorFecha(fecha: string, latitud: number, longitud: number) {
+    this.api.getSismosUSGC(fecha, latitud, longitud).subscribe((data: any) => {
+      try {
+        data.features.forEach((element: any) => {
+
+
+          this.api.getMoreInfo(element.id).subscribe((moreData: any) => {
+            let moreProps = moreData.properties.products.origin[0].properties
+            console.log(moreData);
+            let props = element.properties
+            this.dataUltimosSismos.push({
+              id: element.id,
+              lugar: props.place,
+              magnitud: props.mag,
+              tiempo: new Date(props.time).toLocaleString(),
+              latitud: moreProps.latitude,
+              longitud: moreProps.longitude,
+              profundidad: moreProps.depth
+            })
+          })
+        });
+
+      } catch (err) {
+        console.log('ERROR CON LA CONSULTA');
+      }
+    })
+  }
 
 }
