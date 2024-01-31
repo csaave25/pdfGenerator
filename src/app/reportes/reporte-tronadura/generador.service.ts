a978f69552bafefafdb58533a0b42bab045fe4e5
csaave25 <csaavedra@emt.cl>
2024-01-31 16:07:09 -0300

    revertido

--- src/app/reportes/reporte-tronadura/generador.service.ts
+++ src/app/reportes/reporte-tronadura/generador.service.ts
@@ -406,10 +406,10 @@ export class GeneradorService {
     this.implementarContenidoPDF(inputs)
     setTimeout(() => {
       // this.doc.output('dataurlnewwindow')
-      // var blob = this.doc.output("blob");
-      // window.open(URL.createObjectURL(blob));
-      // this.doc = new jsPDF('p', 'pt', 'letter')
-      this.doc.save('prueba')
+      var blob = this.doc.output("blob");
+      window.open(URL.createObjectURL(blob));
+      this.doc = new jsPDF('p', 'pt', 'letter')
+      // this.doc.save('prueba')
 
     }, 500);
 
