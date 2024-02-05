7592eab74ca8d6b6a94bd2e0833a01eb37bb2655
csaave25 <csaavedra@emt.cl>
2024-02-05 15:57:46 -0300

    map reporte cmdic

--- src/app/reportes/reporte-CMDIC/reporte-cmdic/reporte-cmdic.component.ts
+++ src/app/reportes/reporte-CMDIC/reporte-cmdic/reporte-cmdic.component.ts
@@ -45,43 +45,46 @@ export class ReporteCMDICComponent implements AfterViewInit {
           let height = '1.3rem'
           let mt = '16px'
           let ml = '16px'
+          if (feature.properties.Caudal == null) {
+            color = 'grey'
+            width = '.8rem'
+            height = '.8rem'
+            mt = '10px'
+            ml = '10px'
 
-          if (feature.properties.Caudal > 4.7) {
-            color = '#1161EA'
           } else {
-            if (feature.properties.Caudal <= 4.7 && feature.properties.Caudal > 3.5) {
-              color = '#548CEB'
-              width = '1.2rem'
-              height = '1.2rem'
-              mt = '15px'
-              ml = '15px'
+            if (feature.properties.Caudal > 4.7) {
+              color = '#1161EA'
             } else {
-              if (feature.properties.Caudal <= 3.5 && feature.properties.Caudal > 2.4) {
-                color = '#31A5F0'
-                width = '1.1rem'
-                height = '1.1rem'
-                mt = '13px'
-                ml = '13px'
+              if (feature.properties.Caudal <= 4.7 && feature.properties.Caudal > 3.5) {
+                color = '#548CEB'
+                width = '1.2rem'
+                height = '1.2rem'
+                mt = '15px'
+                ml = '15px'
               } else {
-                if (feature.properties.Caudal <= 2.4 && feature.properties.Caudal > 1.2) {
-                  color = '#7EC0E3'
-                  width = '.9rem'
-                  height = '.9rem'
-                  mt = '11px'
-                  ml = '11px'
+                if (feature.properties.Caudal <= 3.5 && feature.properties.Caudal > 2.4) {
+                  color = '#31A5F0'
+                  width = '1.1rem'
+                  height = '1.1rem'
+                  mt = '13px'
+                  ml = '13px'
                 } else {
-                  if (feature.properties.Caudal <= 1.2) {
-                    color = '#B3D7ED'
-                    width = '.8rem'
-                    height = '.8rem'
-                    mt = '10px'
-                    ml = '10px'
+                  if (feature.properties.Caudal <= 2.4 && feature.properties.Caudal > 1.2) {
+                    color = '#7EC0E3'
+                    width = '.9rem'
+                    height = '.9rem'
+                    mt = '11px'
+                    ml = '11px'
                   } else {
-                    color = 'grey'
-                    width = '.8rem'
-                    height = '.8rem'
-                    mt = '10px'
-                    ml = '10px'
+                    if (feature.properties.Caudal <= 1.2) {
+                      color = '#B3D7ED'
+                      width = '.8rem'
+                      height = '.8rem'
+                      mt = '10px'
+                      ml = '10px'
+                    }
+
                   }
                 }
               }
@@ -108,15 +111,25 @@ export class ReporteCMDICComponent implements AfterViewInit {
             className: "my-custom-pin",
             iconSize: [20, 20],
             html: `<span style="${markerHtmlStyles}" />
-            <p style="${style}">${feature.properties.Id}</p>`
+            <p class="pTag" style="${style}">${feature.properties.Id}</p>`
           })
 
           return leaf.marker(latlng, { icon })
         },
         onEachFeature: (feature: any, layer) => {
-          feature.properties = { ...feature.properties, popupContent: `Nombre: ${feature.properties.Id}` }
-          if (feature.properties && feature.properties.popupContent) {
-            layer.bindPopup(feature.properties.popupContent);
+          if (feature.properties) {
+
+
+            layer.bindPopup(`
+              <div class="container-popup">
+                  <h4 style= 'Font-size: 15px; font-weight: 700;'>${feature.properties.Id}</h4>
+                  <hr style='margin-top: 0' />
+                  <div>  
+                    <p> <span style='font-weight: 600'>Fecha:</span> ${new Date(feature.properties.Fecha).toLocaleDateString('es-ES', { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', })}</p>
+                    <p> <span style='font-weight: 600'>Caudal:</span> ${feature.properties.Caudal}</p>
+                  </div>
+              </div>
+            `);
           }
         },
       }).addTo(this.map);
